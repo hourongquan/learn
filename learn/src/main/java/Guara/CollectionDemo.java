734293ce14d8d55b6dadb2c3b11f2a1e838e91e6
hourongquan <hourongquan@jd.com>
2018-07-07 11:07:26 +0800

    去掉打印

--- learn/src/main/java/Guara/CollectionDemo.java
+++ learn/src/main/java/Guara/CollectionDemo.java
@@ -3,8 +3,8 @@ package Guara;
 public class CollectionDemo {
 
     public static void main(String[] args) {
-       System.out.println("hello");
-        System.out.println("hello");
+      /* System.out.println("hello");
+        System.out.println("hello");*/
         //
 
     }
