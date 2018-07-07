50398a7902bfddfe2df654df1e34bc26c31c1679
hourongquan <40707399+hourongquan@users.noreply.github.com>
2018-07-07 11:14:40 +0800

    Update CollectionDemo.java

--- learn/src/main/java/Guara/CollectionDemo.java
+++ learn/src/main/java/Guara/CollectionDemo.java
@@ -5,5 +5,6 @@ public class CollectionDemo {
     public static void main(String[] args) {
         System.out.println("hello");
         //System.out.println("hello");
+        //把mater分支改为dev的内容
     }
 }
